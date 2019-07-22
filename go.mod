c9938cf8557ab5dce97ad5b140cd1577804804a0
Rajiv Senthilnathan <rajivsen@ca.ibm.com>
2019-07-22 13:17:04 -0400

    First commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.ibm.com/rajivsen/kdo-poc
+
+go 1.12
+
+require github.com/spf13/cobra v0.0.5
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.ibm.com/rajivsen/kdo-poc/common"
+	"strings"
+)
+
+func main() {
+
+	var echoTimes int
+
+	var cmdEcho = &cobra.Command{
+		Use:   "echo [string to echo]",
+		Short: "Echo anything to the screen",
+		Long: `echo is for echoing anything back.
+	Echo works a lot like print, except it has a child command.`,
+		Args: cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Print: " + strings.Join(args, " "))
+		},
+	}
+
+	var cmdTimes = &cobra.Command{
+		Use:   "times [string to echo]",
+		Short: "Echo anything to the screen more times",
+		Long: `echo things multiple times back to the user by providing
+a count and a string.`,
+		Args: cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			for i := 0; i < echoTimes; i++ {
+				fmt.Println("Echo: " + strings.Join(args, " "))
+			}
+		},
+	}
+
+	cmdTimes.Flags().IntVarP(&echoTimes, "times", "t", 1, "times to echo the input")
+
+	var rootCmd = &cobra.Command{Use: "kdo-poc"}
+	rootCmd.AddCommand(common.CmdPrintA, cmdEcho)
+	cmdEcho.AddCommand(cmdTimes)
+	rootCmd.Execute()
+}
